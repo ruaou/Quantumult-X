e3e6b24b5a5ff1786ecf56783d74f89e6e52cc22
ruaou <102359561+ruaou@users.noreply.github.com>
2023-12-14 21:58:07 +0800

    Create Jx.js

---
+++ Jx.js
@@ -0,0 +1,26 @@
+/************
+> 脚本名称：简讯——解锁VIP
+> 软件版本：5.0.16
+> 脚本作者：kira
+> 解锁说明：解锁VIP会员
+> 更新时间：2023-12-14
+> 特别提醒：如需转载请注明出处，谢谢合作！
+> 使用声明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载、售卖
+          ⚠️⚠️⚠️
+
+
+[rewrite_local]
+# > 拦截开屏广告
+^https?:\/\/api\.tipsoon\.com\/api\/v1\/top\/ad url reject-img
+# > 解锁会员
+^https:\/\/api\.tipsoon\.com\/api\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/ruaou/Quantumult-X/CrackVip/Jx.js
+
+[mitm] 
+hostname = api.tipsoon.com
+
+************/
+
+body = $response.body.replace(/\"is_vip":\w+/g, '\"is_vip":true')
+
+$done({body});
