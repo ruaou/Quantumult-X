78a7617dec71df9663a84967059f86d62f64889b
ruaou <102359561+ruaou@users.noreply.github.com>
2023-12-16 23:07:33 +0800

    Update xmlaxx.js

--- xmlaxx.js
+++ xmlaxx.js
@@ -66,21 +66,7 @@ if (body) {
                 console.log("discovery-feed:" + c);
             }
             break;
-
-        case /mobile-user\/v\d\/homePage/.test($request.url):
-            try {
-                let m = new Set([210, 217, 276, 215]),
-                    g = JSON.parse(body);
-                if (g.data.serviceModule.entrances) {
-                    let f = g.data.serviceModule.entrances.filter(e => m.has(e.id));
-                    g.data.serviceModule.entrances = f;
-                }
-                body = JSON.stringify(g);
-            } catch (n) {
-                console.log("mobile-user:" + n);
-            }
-            break;
-
+                        
         default:
             $done({});
     }
